8d9bfc7039a6ca502a60cf6fecfcf034ae6b1f71
Anwesh43 <anweshthecool0@gmail.com>
2018-09-30 12:48:55 +0530

    adding CELNode class

--- CircleEndLineStage.ts
+++ CircleEndLineStage.ts
@@ -37,7 +37,7 @@ class State {
     dir : number = 0
 
     update(cb : Function) {
-        this.scale += 0.025 * this.dir
+        this.scale += 0.05 * this.dir
         if (Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir
             this.dir = 0
@@ -70,5 +70,72 @@ class Animator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class CELNode {
+    next : CELNode
+    prev : CELNode
+    state : State = new State()
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new CELNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = h / (nodes + 1)
+        const r : number = gap / 3
+        context.strokeStyle = '#388E3C'
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / 60
+        context.save()
+        context.translate(w/2, gap * this.i + gap)
+        for (var j = 0; j < 2; j++) {
+            const sf : number = 1 - 2 * j
+            const sc : number = Math.min(0.5, Math.max(0, this.state.scale - 0.5 * j)) * 2
+            context.save()
+            context.scale(sf, 1)
+            context.translate(w/2 * sc, 0)
+            context.rotate(Math.PI * sc)
+            context.beginPath()
+            for (var k = 180; k <= 360; k++) {
+                const x : number = r * Math.cos(k * Math.PI/180)
+                const y : number = r * Math.sin(k * Math.PI/180)
+                if (k == 180) {
+                    context.moveTo(x, y)
+                } else {
+                    context.lineTo(x, y)
+                }
+            }
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) {
+        var curr : CELNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
